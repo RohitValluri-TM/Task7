50ee5f04d8b4227574b0f88cb3e31be1820b0c35
Rohit Valluri <rohit.valluri@techmojo.in>
2022-08-05 16:50:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import {useState} from "react"
+import productData from './products.json'
 import './App.css';
+import { Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Product(){
+    
+    const copy = productData.products
+    const [data, setData] = useState(copy)
+    
+    return(
+        <>
+        
+        <h1>Products</h1>
+       
+        {data.map((ele, index)=>{
+            return(
+                <div className="cd" style={{display:'inline-grid'}} key={index}>
+                    <div className="card">
+                        <img className="card-img-top" src={ele.images[0]} alt="Card image cap"/>
+                        <div className="card-body">
+                            <h4 className="card-title">{ele.title}</h4>
+                            <h4>Rating : {ele.rating}</h4>
+                            <h4>Price : ${ele.price}</h4>
+                           <Link to="/ProductDetails" state={{info:ele}}> <button className="btn btn-primary stretched-link">More Details ...</button></Link>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+        }
+        </>
+    )
 }
-
-export default App;
---
+++ src/ProductDetails.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+
+function ProductDetails() {
+    const location = useLocation()
+    const {info} = location.state
+  return (
+    <div>
+        <h2>ProductDetails</h2>
+        <div className='left'>                                                                   
+            <img id="pdimg" alt="productimg" src={info.images[0]}></img>
+        </div>
+        <div className='right'>
+        <div className="product-info">
+            <h2>Product : {info.title}</h2>
+            <h2>Price : ${info.price}</h2>
+            <h2>Brand : {info.brand}</h2>
+            <h2>Rating : {info.rating}</h2>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDetails
